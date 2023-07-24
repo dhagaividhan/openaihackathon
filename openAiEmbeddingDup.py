1e24a7973125b8058d8d3e5fb3a33e73d0a47694
Vidhan Dhagai <77140850+dhagaividhan@users.noreply.github.com>
2023-07-24 14:45:03 +0530

    Update openAiEmbeddingDup.py

--- openAiEmbeddingDup.py
+++ openAiEmbeddingDup.py
@@ -18,3 +18,13 @@ similarity_score_ab = np.dot(embedding_a, embedding_b)
 similarity_score_ac = np.dot(embedding_a, embedding_c)
 print("similarity score of disc1 and disc2:  " + str(similarity_score_ab))
 print("similarity score of disc1 and disc3:  " + str(similarity_score_ac))
+
+# what i did:
+# disc1 is from openai chat playground after asking the question as: 'Give me sample disclaimer or disclosure in a paragraph format to be printed in a trade confirmation for a fixed income security bond'
+# Answer: "This trade confirmation is for informational purposes only and does not constitute an offer or solicitation to buy or sell securities. The information provided herein is based on current market conditions and prices, which are subject to change without notice. The accuracy and completeness of the information cannot be guaranteed. The bond being confirmed is subject to the terms and conditions outlined in the official offering documents, including the prospectus and/or indenture. Investors should carefully review these documents before making any investment decisions. Past performance is not indicative of future results. The firm makes no representations or warranties regarding the suitability or appropriateness of this investment for any particular investor. The firm may act as principal, agent, or market-maker in the securities mentioned in this confirmation, which may create a conflict of interest. Investors should consult with their own financial advisors or professionals before making any investment decisions. This confirmation is intended solely for the named recipient and may not be reproduced or redistributed without the firm's prior written consent."
+
+# I compared this answer with disc2 which was an actual disclosure for fixed income securities bonds for CHARLES SCHWAB.. output I got is:
+# similarity score of disc1 and disc2:  0.7504109050177736
+
+# When I compared it with disc3 which is for AMERICAN INC FD output is:
+# similarity score of disc1 and disc3:  0.7639821565214152
